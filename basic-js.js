f4534ba603b94efe79cc43b2d2a22ae4a0fd4e17
arpita <arpitaagrahari004@gmail.com>
2024-08-12 10:44:30 +0530

    Add initial JavaScript code and package configuration files

---
+++ basic-js.js
@@ -0,0 +1,13 @@
+//Global var 
+var global = "I am global"
+console.log(global);
+
+//local 
+function myLocal() {
+    var local = "I am local"
+    console.log(local);
+}
+//local();
+//console.log(local); //error :  diff btw local and global ...
+// local is declared inside the function so it is local to the function
+// global is declared outside the function so it is global to the function
