8daedc081fb321ea99d9fdcc45f7e8a9d9f528ec
arpita <arpitaagrahari004@gmail.com>
2024-08-22 16:24:00 +0530

    Refactor JavaScript code for better readability and remove unnecessary comments

--- basic-js.js
+++ basic-js.js
@@ -304,3 +304,24 @@
 // }
 // const car = new Model("Ford", "Mustang");
 // console.log(car.show());
+
+
+
+
+//Array methods in ES6
+//concat
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// const arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+
+//filter
+// const arr = [1, 2, 3, 4, 5];
+// const arr1 = arr.filter((item) => item > 2);
+// console.log(arr1);
+
+
+const arr = [1, 2, 3, 4, 5];
+const even = arr.filter((item) => item % 2 === 0);
+console.log(even);
