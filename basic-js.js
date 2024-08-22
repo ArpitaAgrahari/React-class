0df54f4809ed767a274aafaf65ceafa875483f79
arpita <arpitaagrahari004@gmail.com>
2024-08-22 16:18:49 +0530

    Refactor JavaScript code to implement class inheritance

--- basic-js.js
+++ basic-js.js
@@ -284,3 +284,23 @@
 
 
 //inheritance in class
+class Car{
+    constructor(brand){
+        this.brand = brand;
+    }
+    display(){          //return a string which will describe the car
+        return "I am a " + this.brand;
+    }
+}
+
+class Model extends Car{
+    constructor(brand, model){
+        super(brand);
+        this.model = model;
+    }
+    show(){
+        return this.display() + " " + this.model;
+    }
+}
+const car = new Model("Ford", "Mustang");
+console.log(car.show());
