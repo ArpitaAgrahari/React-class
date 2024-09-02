6d363a0ae5e71df3f52694c0be250c703eb66f1f
arpita <arpitaagrahari004@gmail.com>
2024-09-02 11:16:02 +0530

    chore: Refactor App component for improved readability and remove commented out code

--- Pratice1/src/App.jsx
+++ Pratice1/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  return (
-    <div>
+const simpleComponent=()=>{
+  const handleClick=()=>{
+    const message=document.getElementById('message');
+    message.textContent='Hello, React!';
+  };
+  return(  
+      <div>
       <h1>React</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={handleClick}>Click</button>
+      <p id="message"></p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default simpleComponent;
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>React</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//     </div>
+//   )
+// }
+
+// export default App
