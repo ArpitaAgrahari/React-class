68bc7fd728558f7aacd185487961162da0bd7fbc
arpita <arpitaagrahari004@gmail.com>
2024-09-05 15:32:41 +0530

    Refactor App component for improved readability and remove unnecessary code

--- Pratice1/src/App.jsx
+++ Pratice1/src/App.jsx
@@ -1,55 +1,71 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
+// import './App.css'
 
-// const simpleComponent=()=>{
-//   const handleClick=()=>{
-//     const message=document.getElementById('message');
-//     message.textContent='Hello, React!';
-//   };
-//   return(  
-//       <div>
-//       <h1>React</h1>
-//       <h2>Start editing to see some magic happen!</h2>
-//       <button onClick={handleClick}>Click</button>
-//       <p id="message"></p>
-//     </div>
-//   );
-// };
+// // const simpleComponent=()=>{
+// //   const handleClick=()=>{
+// //     const message=document.getElementById('message');
+// //     message.textContent='Hello, React!';
+// //   };
+// //   return(  
+// //       <div>
+// //       <h1>React</h1>
+// //       <h2>Start editing to see some magic happen!</h2>
+// //       <button onClick={handleClick}>Click</button>
+// //       <p id="message"></p>
+// //     </div>
+// //   );
+// // };
 
 
-// export default simpleComponent;
+// // export default simpleComponent;
 
-// function App() {
-//   return (
+// // function App() {
+// //   return (
+// //     <div>
+// //       <h1>React</h1>
+// //       <h2>Start editing to see some magic happen!</h2>
+// //     </div>
+// //   )
+// // }
+
+// // export default App
+
+
+// function JsxComponent(){
+//   return(
+//     //rule 1 :: enclose html in div tag (mandatory) 
 //     <div>
-//       <h1>React</h1>
-//       <h2>Start editing to see some magic happen!</h2>
+//           <h1>Hello react!!</h1>
+//           <p>Start editing to see some magic happen!</p>
+//           <img src="https://i.pinimg.com/originals/be/a1/10/bea11075078b1a2992c78596ff40e05a.jpg"></img>
+//           {/* //rule 2 :: close every tag (mandatory)
+//                  //fragments */}
+//           <br></br>
+//           <ul>
+//             <li>React</li>
+//             <li>Hi</li>
+//           </ul>
+//           <button onClick={()=>alert('Hey dont click me!!! Understood??')}>Click</button>
 //     </div>
-//   )
+//     //rule 3:: use camelCase for html attributes
+
+//   );
 // }
 
-// export default App
+// export default JsxComponent;
 
 
-function JsxComponent(){
-  return(
-    //rule 1 :: enclose html in div tag (mandatory) 
-    <div>
-          <h1>Hello react!!</h1>
-          <p>Start editing to see some magic happen!</p>
-          <img src="https://i.pinimg.com/originals/be/a1/10/bea11075078b1a2992c78596ff40e05a.jpg"></img>
-          {/* //rule 2 :: close every tag (mandatory)
-                 //fragments */}
-          <br></br>
-          <ul>
-            <li>React</li>
-            <li>Hi</li>
-          </ul>
-          <button onClick={()=>alert('Hey dont click me!!! Understood??')}>Click</button>
-    </div>
-    //rule 3:: use camelCase for html attributes
 
+import React from 'react';
+const test=()=>{
+
+  return React.createElement(
+    //types of element
+    //properties or object 
+    //children of elem
+    'div',
+    null,
+    '<h1>Hello react!!</h1>',
   );
 }
-
-export default JsxComponent;
+export default test;
