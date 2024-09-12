b58df54bf87a753d2db9907359b9c239d5692b57
arpita <arpitaagrahari004@gmail.com>
2024-09-12 15:26:49 +0530

    Refactor index.js to update App component and add click event handler

--- Pratice2/src/index.js
+++ Pratice2/src/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+import { useState } from "react";
+function App(){
+    let count=0;
+    function handleApp(){
+        count+=1;
+        console.log(count);
+    }
+    return (
+        <div>
+        <p>{count}</p>
+        <button onClick={handleApp}>Add</button>
+        <button>Sub</button>
+        </div>
+    )
+}
