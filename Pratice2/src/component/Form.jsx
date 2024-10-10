0f091dda6ae2f5df5b9e2c5497f85239f5387030
arpita <arpitaagrahari004@gmail.com>
2024-10-10 16:28:50 +0530

    Refactor Form component to add useFormInput custom hook for handling form input state

---
+++ Pratice2/src/component/Form.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+const useFormInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+    const reset = () => {
+        setValue('');
+    }
+    return (
+        <div>
+            <form>
+                <label>Name:</label>
+                <input type="text" value={value} onChange={handleChange} />
+                <label>Email:</label>
+                <input type="text" value={value} onChange={handleChange} />
+            </form>
+        </div>
+    )
+}
+export default useFormInput;
