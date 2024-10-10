8e97b37b087d5f59d736b0b19c79071fab80f0a6
arpita <arpitaagrahari004@gmail.com>
2024-10-10 16:07:05 +0530

    Refactor useMemo component to add missing useState import and define missing setCount variable

--- Pratice2/src/component/useMemo.js
+++ Pratice2/src/component/useMemo.js
@@ -1,7 +1,24 @@
-import React from "react";
-const [count, setCount] = useState(0);
+import { useMemo, useState } from 'react';
 
-const resultFun=(num)=>{
-    console.log("resultFun");
-    return num+1;
-}
+const UseMemo = () => {
+    const [count, setCount] = useState(0);
+
+    const result = (num) => {
+        console.log("Result is...");
+        return num * 12;
+    };
+
+    const value = useMemo(() => {
+        return result(count);
+    }, [count]);
+
+    return (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={() => setCount(count + 1)}>Increment Count</button>
+            <h2>Count multiplied by 12 is {value}</h2>
+        </div>
+    );
+};
+
+export default UseMemo;
