0b59d547a13195ebcdf3170e5021fbd28b341584
arpita <arpitaagrahari004@gmail.com>
2024-12-02 00:53:33 +0530

    Remove LoanCalculator and PersonalInfoForm components, and clean up App component and CSS

--- Pratice1/src/App.jsx
+++ Pratice1/src/App.jsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import LoanCalculatorPage from './pages/LoanCalculatorPage';
-import PersonalInfoPage from './pages/PersonalInfoPage';
 import './App.css';
 
 function App(){
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/loan-calculator">Loan Calculator</Link>
-          <Link to="/personal-info-form">Personal Info Form</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/loan-calculator" element={<LoanCalculatorPage />} />
-          <Route path="/personal-info-form" element={<PersonalInfoPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+    
+    </>
   );
 }
 
